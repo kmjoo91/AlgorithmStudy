ee82e3d8035148733bf6585058e1145e95aba7b1
kim-minjoo <kim.minjoo@nhn.com>
2018-01-10 16:50:38 +0900

    동전거스름문제

---
+++ src/test/java/com/naver/divideandconquer/coin/CoinTest.java
@@ -0,0 +1,36 @@
+package com.naver.divideandconquer.coin;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CoinTest {
+	@Test
+	public void CoinChangeProblemTest1() {
+		//given
+		int[] coins = {10, 100, 500, 50};
+		int expected = 4;
+
+		//when
+		int actual = Coin.calculate(170, coins);
+
+		//then
+		assertEquals(expected, actual);
+	}
+
+	/**
+	 * 최적해가 아닌 경우 테스트
+	 * 120원 2개가 최적해.
+	 */
+	@Test
+	public void CoinChangeProblemTest2() {
+		int[] coins = {10, 120, 100, 200, 50};
+		int expected = 2;
+
+		//when
+		int actual = Coin.calculate(240, coins);
+
+		//then
+		assertEquals(expected, actual);
+	}
+}
