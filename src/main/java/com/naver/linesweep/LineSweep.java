94c2cea29b7c5ee96ea27a7031e8edd5e1786793
kim.minjoo <nt11201@nhn.com>
2018-11-20 17:58:21 +0900

    LineSweep 알고리즘 구현

--- src/main/java/com/naver/linesweep/LineSweep.java
+++ src/main/java/com/naver/linesweep/LineSweep.java
@@ -6,50 +6,44 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class LineSweep {
-
-	public LineSweep(Point[] points) {
-	}
-
 	public double calculateDistance(Point[] points) {
 		Arrays.sort(points, new PointXComparator());
 
 		double minDistance = getDistance(points[0], points[1]);
-		int start = 0;
 
-		List<Point> possiblePoints = new ArrayList<>();
-		possiblePoints.add(points[0]);
-		possiblePoints.add(points[1]);
+		Point[] possiblePoints = {points[0], points[1]};
 
+		int possibleIndex = 0;
 		for (int i = 2; i < points.length; i++) {
 			Point currentPoint = points[i];
-			for (int j = start; j < i; j++) {
-				double xDistance = currentPoint.getX() - points[j].getX();
-				//클때
-				if (xDistance > minDistance) {
-					possiblePoints.remove(points[j]);
-				}
-				//작을때
-				else {
-					start = j;
+
+			for (int j = possibleIndex; j < i; j++) {
+				Point point = points[j];
+				double xDistance = currentPoint.getX() - point.getX();
+
+				if (xDistance < minDistance) {
+					possibleIndex = j;
 					break;
 				}
 			}
 
 			double lowerY = currentPoint.getY() - minDistance;
-			double upperY = currentPoint.getY() - minDistance;
+			double upperY = currentPoint.getY() + minDistance;
 
-			possiblePoints = possiblePoints.stream()
-					.filter(point -> point.getY() >= lowerY && point.getY() <= upperY)
-					.collect(Collectors.toList());
+			possiblePoints = Arrays.copyOfRange(points, possibleIndex, i);
+
 
 			for (Point point : possiblePoints) {
+				if (point.getY() < lowerY || point.getY() > upperY) {
+					continue;
+				}
+
 				double distance = getDistance(currentPoint, point);
 				if (distance < minDistance) {
 					minDistance = distance;
 				}
 			}
 
-			possiblePoints.add(currentPoint);
 		}
 
 		return minDistance;
